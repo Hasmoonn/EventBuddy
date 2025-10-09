b15f770bddbcf69bad4ad9d63ff28d5feb35bf82
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-10-09 21:34:45 +0530

    Add useEffect for 404 error logging

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -3,4 +3,8 @@ import { useLocation } from 'react-router-dom';
 
 const NotFound = () => {
   const location = useLocation();
+
+  useEffect(() => {
+    console.error("404 Error: User attempted to access non-existent route:", location.pathname);
+  }, [location.pathname]);
 }
