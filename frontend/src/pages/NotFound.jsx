06a0de1aaf6228f1c44ff294172f01f59579131b
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-10-09 21:32:27 +0530

    Add NotFound component structure with imports and useLocation hook

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,9 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useLocation } from 'react-router-dom';
 
 const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  )
+  const location = useLocation();
 }
-
-export default NotFound
