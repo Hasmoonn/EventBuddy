4a25f3a728dd2f01a62a00eaa643263171705437
Hasmoonn <mohamedhasmoon175@gmail.com>
2025-11-10 17:53:07 +0530

    review model is developed with adding new attributes like booking_id, rating, comment

--- backend/models/reviewModel.js
+++ backend/models/reviewModel.js
@@ -3,8 +3,9 @@ import mongoose from 'mongoose';
 const reviewSchema = new mongoose.Schema({
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   vendor_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Vendor', required: true },
-  rating: { type: Number, min: 1, max: 5 },
-  comment: String
+  booking_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Booking', required: true },
+  rating: { type: Number, min: 1, max: 5, required: true },
+  comment: { type: String, required: true }
 }, { timestamps: true });
 
 const reviewModel = mongoose.models.Review || mongoose.model('Review', reviewSchema);
