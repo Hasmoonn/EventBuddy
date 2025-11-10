cf053506582a32addb926786cb6d7fe1c2f59dc6
Hasmoonn <mohamedhasmoon175@gmail.com>
2025-11-10 17:50:28 +0530

    event_id, service_description attribute is added

--- backend/models/bookingModel.js
+++ backend/models/bookingModel.js
@@ -3,10 +3,11 @@ import mongoose from "mongoose";
 const bookingSchema = new mongoose.Schema({
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   vendor_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Vendor', required: true },
+  event_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true },
   booking_date: { type: Date, required: true },
   total_amount: { type: Number },
   status: { type: String, enum: ['Pending', 'Confirmed', 'Cancelled'], default: 'Pending' },
-  service_description: String,
+  service_description: { type: String },
   notes: { type: String }
 }, { timestamps: true });
 
