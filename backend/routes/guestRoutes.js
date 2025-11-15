22e38600aee85b9989bcfe59e106ef43a8309204
Oshada Nethmina <oshadanethmina665@gmail.com>
2025-11-15 14:39:31 +0530

    add guest router with CRUD operations for event guests

---
+++ backend/routes/guestRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { getEventGuests, createGuest, updateGuest, deleteGuest } from "../controllers/guestController.js";
+import { userAuth } from "../middleware/userAuth.js";
+
+const guestRouter = express.Router();
+
+guestRouter.get("/:eventId", userAuth, getEventGuests);
+
+guestRouter.post("/:eventId", userAuth, createGuest);
+
+guestRouter.put("/:eventId/:guestId", userAuth, updateGuest);
+
+guestRouter.delete("/:eventId/:guestId", userAuth, deleteGuest);
+
+export default guestRouter;
