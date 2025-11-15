31db737b81efa6cef8691248e6856c8cd6fb7e09
Dulari Dayananda <dulariwathsala824@gmail.com>
2025-11-15 18:00:32 +0530

    Add basic vendors page component with imports

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Calendar, Mail, MapPin, MessageCircle, Phone, Search, Star } from 'lucide-react'
+import toast from 'react-hot-toast';
+import {categoryIcons} from '../assets/assets.js'
+import axios from "axios"
+import { AuthContext } from '../contexts/AuthContext.jsx';
+import { HashLoader } from 'react-spinners';
 
-const About = () => {
+const Vendors = () => {
+
+  const {backendUrl, user, isAuthenticated, loading, setLoading} = useContext(AuthContext);
+
   return (
-    <div>About</div>
+    <div className="min-h-screen bg-gradient-to-r from-[rgb(var(--accent),0.5)] to-[rgba(var(--accent-foreground),0.1)]">
+      <h1>Vendors Page</h1>
+    </div>
   )
 }
 
-export default About
+export default Vendors;
