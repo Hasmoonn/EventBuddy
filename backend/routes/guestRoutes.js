b11eaf8d1c968d916511aea60359847e6a85e5bb
Oshada Nethmina <oshadanethmina665@gmail.com>
2025-11-17 19:40:17 +0530

    Add full guest management routes with userAuth protection

--- backend/routes/guestRoutes.js
+++ backend/routes/guestRoutes.js
@@ -3,5 +3,11 @@ import { getEventGuests, createGuest, updateGuest, deleteGuest} from  "../contro
 import {userAuth} from "../middleware/userAuth.js";
 import guestRouter from '../../../../../Downloads/backend-oshada/routes/guestRoutes';
 
+const guestRouter = express.Router();
+
+guestRouter.get('/:eventId', userAuth, getEventGuests);
+guestRouter.post('/:eventId', userAuth, createGuest);
+guestRouter.put('/:eventId/:guestId', userAuth, updateGuest);
+guestRouter.delete('/:eventId/:guestId', userAuth, deleteGuest);
 
 export default guestRouter;
