e7072ebcd2ab9a72573bee6b177a7fd04b3d75db
Oshada Nethmina <oshadanethmina665@gmail.com>
2025-11-17 19:38:41 +0530

    Fix guest routes import and reorganize guest router export

--- backend/routes/guestRoutes.js
+++ backend/routes/guestRoutes.js
@@ -1,15 +1,7 @@
-import express from "express";
-import { getEventGuests, createGuest, updateGuest, deleteGuest } from "../controllers/guestController.js";
-import { userAuth } from "../middleware/userAuth.js";
+import express from 'express';
+import { getEventGuests, createGuest, updateGuest, deleteGuest} from  "../controllers/guestController.js";
+import {userAuth} from "../middleware/userAuth.js";
+import guestRouter from '../../../../../Downloads/backend-oshada/routes/guestRoutes';
 
-const guestRouter = express.Router();
-
-guestRouter.get("/:eventId", userAuth, getEventGuests);
-
-guestRouter.post("/:eventId", userAuth, createGuest);
-
-guestRouter.put("/:eventId/:guestId", userAuth, updateGuest);
-
-guestRouter.delete("/:eventId/:guestId", userAuth, deleteGuest);
 
 export default guestRouter;
