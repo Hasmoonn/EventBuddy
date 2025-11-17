6d059b926a00317b84a107b6aa494bc5e933dc3b
Oshada Nethmina <oshadanethmina665@gmail.com>
2025-11-17 19:49:18 +0530

    Add authenticated event routes for creating, fetching, updating, and deleting events

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -2,6 +2,13 @@ import express from "express";
 import { createEvent, getEventById, updateEvent, getUserEvents, deleteEvent } from "../controllers/eventController.js";
 import {userAuth} from "../middleware/userAuth.js";
 
+const eventRouter = express.Router();
 
+eventRouter.post("/", userAuth, createEvent);
+eventRouter.get("/user", userAuth, getUserEvents);
+eventRouter.put("/:id", userAuth, updateEvent);
+eventRouter.delete("/:id", userAuth, deleteEvent);
+
+eventRouter.get("/:id", userAuth, getEventById);
 
 export default eventRouter;
