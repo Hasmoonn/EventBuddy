c587820a3a300a7e471e4d5536715cef1d7f7fd2
Oshada Nethmina <oshadanethmina665@gmail.com>
2025-11-17 07:27:52 +0530

    Add review routes setup and import controllers/middleware

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -1,14 +1,8 @@
-import express from "express";
-import { userAuth } from "../middleware/userAuth.js";
-import { getVendorReviews, submitReview, getUserBookings, editReview, deleteReview } from "../controllers/reviewController.js";
+import express from 'express';
+import { userAuth } from '../middleware/userAuth.js';
+import { getVendorReviews, submitReview, getUserBookings, editReview, deleteReview } from '../controllers/reviewController.js';
 
 const reviewRouter = express.Router();
 
-reviewRouter.get("/vendor/:vendorId", getVendorReviews);
-
-reviewRouter.post("/submit", userAuth, submitReview);
-reviewRouter.get("/user-bookings", userAuth, getUserBookings);
-reviewRouter.put("/:reviewId", userAuth, editReview);
-reviewRouter.delete("/:reviewId", userAuth, deleteReview);
 
 export default reviewRouter;
