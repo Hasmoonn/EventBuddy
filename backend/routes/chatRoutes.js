9a005f3d386ea3747f8b07a240d5914ada01c2c2
Hasmoonn <mohamedhasmoon175@gmail.com>
2025-12-18 13:24:12 +0530

    removed and commented

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { chatProxy, getSuggestions } from '../controllers/chatController.js';
+import { chatProxy } from '../controllers/chatController.js';
 import { userAuth } from '../middleware/userAuth.js';
 
 const router = express.Router();
 
-router.get('/suggestions', getSuggestions);
+// router.get('/suggestions', getSuggestions);
 router.post('/', userAuth, chatProxy);
 
 export default router;
